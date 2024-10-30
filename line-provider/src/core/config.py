29e033b74a25e042e67782282f40c2a69aa87c77
Dmitry Sergeev <dmitry.sergeev@nordclan.com>
2024-10-30 10:12:39 +0300

    Актуализировал конфиг

--- line-provider/src/core/config.py
+++ line-provider/src/core/config.py
@@ -18,7 +18,7 @@ class DatabaseConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=("../../.env.template", "../../.env"),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="LINE_PROVIDER_CONFIG__"
