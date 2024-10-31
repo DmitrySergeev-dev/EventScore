182898b011c2b4b29e2832ea403ce6764bdb0eec
Dmitry Sergeev <dmitry.sergeev@nordclan.com>
2024-10-31 10:01:37 +0300

    # line-provider Удалил неиспользуемые модули

--- line-provider/src/core/connectors.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def get_redis_host_and_port():
-    host = os.environ.get("REDIS_HOST", "localhost")
-    port = 63791 if host == "localhost" else 6379
-    return dict(host=host, port=port)
