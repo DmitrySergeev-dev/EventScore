373f756adee7345719d5b00b92d43cc49b536326
Dmitry Sergeev <dmitry.sergeev@nordclan.com>
2024-11-19 18:02:38 +0300

    #score-maker Сгенерировал миграцию

---
+++ score-maker/migrations/versions/0.0.0.0_create_tables.py
@@ -0,0 +1,73 @@
+"""Создание таблиц
+
+Revision ID: 0.0.0.0
+Revises: 
+Create Date: 2024-11-19 17:50:38.634738
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = '0.0.0.0'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION public.uuid_generate_v4()
+        RETURNS uuid
+        LANGUAGE 'c'
+            COST 1
+            VOLATILE STRICT PARALLEL SAFE 
+            AS '$libdir/uuid-ossp', 'uuid_generate_v4'
+        ;
+        """
+    )
+    op.execute("CREATE SCHEMA IF NOT EXISTS news")
+    op.create_table(
+        'news_score',
+        sa.Column(
+            'pk',
+            sa.Text(),
+            server_default=sa.text('public.uuid_generate_v4()'),
+            nullable=False
+        ),
+        sa.Column(
+            'news_id',
+            sa.Text(),
+            nullable=False,
+            comment='Идентификатор новости'
+        ),
+        sa.Column(
+            'score',
+            sa.Integer(),
+            nullable=True,
+            comment='Оценка новости'
+        ),
+        sa.Column(
+            'editable',
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.false(),
+            comment='Возможность редактирования оценки'
+        ),
+        sa.CheckConstraint(
+            'score <= 5 AND score >= 1',
+            name='score_value_limit'
+        ),
+        sa.PrimaryKeyConstraint('pk'),
+        schema='news',
+        comment='Оценки новостей'
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('news_score', schema='news')
+    op.execute("DROP SCHEMA IF EXISTS news CASCADE;")
+    op.execute("DROP FUNCTION IF EXISTS public.uuid_generate_v4();")
