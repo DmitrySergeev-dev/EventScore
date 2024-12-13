3455d47678b3d2046c1af0ae9f494aeac20be6e4
Dmitry Sergeev <dmitry.sergeev@nordclan.com>
2024-12-13 12:51:40 +0300

    # frontend фикс ошибок при работе с fetch

---
+++ frontend/src/components/UI/MyDialog.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps(
+    {
+      show: {
+        type: Boolean,
+        required: true,
+      default: false
+      },
+    }
+)
+
+const emit = defineEmits(["hideDialog"])
+
+
+</script>
+
+<template>
+  <div class="dialog" v-if="show" @click.stop="$emit('hideDialog')">
+    <div @click.stop class="dialog__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.dialog {
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+}
+
+.dialog__content {
+  margin: auto;
+  background: white;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 300px;
+  padding: 20px;
+}
+</style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -3,8 +3,11 @@ import {ref, onMounted} from 'vue'
 import {fetchNews, createNews, deleteNews} from '@/api/news.ts'
 import NewsList from '@/components/NewsList.vue'
 import NewsForm from '@/components/NewsForm.vue'
+import MyButton from '@/components/UI/MyButton.vue'
+import MyDialog from '@/components/UI/MyDialog.vue'
 
 const news = ref([])
+const dialogVisible = ref(false)
 
 onMounted(async () => {
   news.value = await fetchNews()
@@ -12,6 +15,7 @@ onMounted(async () => {
 
 const addNewsItem = async (newsItem) => {
   await createNews(newsItem)
+  dialogVisible.value = false
   news.value = await fetchNews()
 }
 
@@ -19,9 +23,23 @@ const removeNewsItem = async (newsItem) => {
   await deleteNews(newsItem.pk)
   news.value = await fetchNews()
 }
+
+const showDialog = async () => {
+  dialogVisible.value = true
+  console.log(`dialogVisible: ${dialogVisible.value}`)
+}
+
+function hideDialog() {
+  dialogVisible.value = false
+}
+
 </script>
 
 <template>
-  <NewsForm @newsCreated="addNewsItem"/>
-  <NewsList :news="news" @removeNewsItem="removeNewsItem"/>
+  <my-button @click="showDialog">Добавить новость</my-button>
+  <my-dialog :show="dialogVisible" @hideDialog="hideDialog">
+    <news-form @newsCreated="addNewsItem" />
+  </my-dialog>
+  <NewsList :news="news" @removeNewsItem="removeNewsItem" />
 </template>
+
